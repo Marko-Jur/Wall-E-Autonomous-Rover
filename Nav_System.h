361decf9e4c701577de712e8cbca803ce4a4b72f
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-19 15:54:27 -0800

    Initial files
    Initial files

---
+++ Nav_System.h
@@ -0,0 +1,25 @@
+/* Pin Assignments
+ *  
+ *  Created by: Marko Jurisic
+ *  Date: 17th December 2020
+ *  Purpose: Contains all pin references for Wall-E autonomous driving code
+ */
+
+#ifndef _PINASSIGNMENTS_H    
+#define _PINASSIGNMENTS_H    
+
+
+void setupNav(){
+
+Serial.println("fuckoff");
+
+return 0;
+
+}
+/*
+void setupReceiver(){
+
+}
+*/
+
+ #endif
