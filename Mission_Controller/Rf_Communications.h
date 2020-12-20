8337ec20fab11c6bb76c5aa41085e17d9ed4d816
PigeonOverlord <janithwijekoon@gmail.com>
2020-12-19 17:46:24 -0800

    bleh bleh bleh

---
+++ Mission_Controller/Rf_Communications.h
@@ -0,0 +1,18 @@
+/*  RF Communications
+ *  
+ *  Created by: Janith Wijekoon
+ *  Date: 19th December 2020
+ *  Purpose: Hold constants and variables necessary for RF communication.
+ */
+
+#ifndef _RF_COMMS_H    
+ #define _RF_COMMS_H    
+
+ //variables
+ 
+ const float TERMINATE_DISTANCE = 1.00;
+
+ //function delarations 
+
+ void setupRfCommunications(int mode);
+#endif
