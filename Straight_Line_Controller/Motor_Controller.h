c21367878e4ce96f1e28b4f6be544d28b32d510e
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-23 15:28:28 -0800

    Makes Wall-E go in a straight line

---
+++ Straight_Line_Controller/Motor_Controller.h
@@ -0,0 +1,18 @@
+/*Function: Motor Controller
+ *Author: Marko Jurisic
+ *Input Parameters: Bearing, Heading
+ *Output Parameters: None
+ *
+ *Purpose: Initializes Wall-E with the target location from the GCS, and then Wall-E begins driving to the location while reporting it's
+ *         poisitional data to the GCS
+ */
+
+#ifndef _MOTORCONTROLLER_H    
+#define _MOTORCONTROLLER_H    
+
+//function declarations 
+void motorController(float bearing, float heading, float distance);
+void motorSetup();
+void encoderRead();
+
+#endif
